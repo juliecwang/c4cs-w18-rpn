3221259ecd4960903f46947eca247b055b9c83af
Julie Wang <jwang135@gmail.com>
2018-03-06 21:59:38 -0500

    Added other operators

--- rpn.py
+++ rpn.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+import operator 
+
+
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
@@ -7,13 +18,21 @@ def calculate(arg):
             value = int(token)
             stack.append(value)
         except ValueError:
+	    function = operators[token]	
             arg1 = stack.pop()
             arg2 = stack.pop()
-            return arg1+arg2
+            return function(arg1, arg2)
+	    stack.append(result)
+	print(stack)
+    if len(stack) != 1:
+	raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-    while True:
-          print(calculate(input('rpn calc> ')))
+	while True:
+		result = calculate(input("rpn calc> "))
+		print("Result: ", result)
+
 
 if __name__ == '__main__':
      main()
